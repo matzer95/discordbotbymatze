936907089e5325f1631f88577f31259f5ef89664
MatzeR95 <matzer.95@gmail.com>
2019-12-21 16:22:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const fetch = require('node-fetch');
 const querystring = require('querystring');
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
-//const fetch = require('node-fetch');
+
 
 
 
