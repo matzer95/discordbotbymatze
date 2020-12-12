6b1cdbd9b5fc157145cb80ffa6449417259ccfa1
MatzeR95 <matzer.95@gmail.com>
2020-12-12 21:35:28 +0100

    first in a while

--- bot.js
+++ bot.js
@@ -5,29 +5,41 @@ const fetch = require('node-fetch');
 const querystring = require('querystring');
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
+const {google} = require('googleapis');
+const sheets = google.sheets('v4');
+
 const prefix = '!';
 
 var latest_vid_id = 'VID_ID';
-var mastery5 = [];
-var i;
+var xyz;
 
 client.once('ready', () => {
     console.log('I am ready!');
 });
 
+// https://sheets.googleapis.com/v4/spreadsheets/1Wqlovr44XVOGj8dimevZnUbgRk7OX8rfFDSexGARqwo/values/'General%20Stats'!B5:D16?key=
+//GGL_TOKEN
+
+//client.on('ready', message => {
+//	client.channels.get('657656218281705503').send("loop");
+//	let url = 'https://www.googleapis.com/youtube/v3/search?key='+process.env.GGL_TOKEN+'&channelId='+process.env.CHANNEL_ID+'&part=id&order=date&maxResults=1'
+//	fetch(url)
+//	.then(res => res.json())
+//	.then((out) => {
+//		if (out.items[0].id.videoId !== latest_vid_id) {
+//			latest_vid_id = out.items[0].id.videoId;
+//		};
+//	});
+//});
 
 client.on('ready', message => {
-	client.channels.get('657656218281705503').send("loop");
-	let url = 'https://www.googleapis.com/youtube/v3/search?key='+process.env.GGL_TOKEN+'&channelId='+process.env.CHANNEL_ID+'&part=id&order=date&maxResults=1'
+	let url = 'https://sheets.googleapis.com/v4/spreadsheets/1Wqlovr44XVOGj8dimevZnUbgRk7OX8rfFDSexGARqwo/values/\'General%20Stats\'!B5:D16?key='+process.env.GGL_TOKEN
 	fetch(url)
 	.then(res => res.json())
 	.then((out) => {
-		if (out.items[0].id.videoId !== latest_vid_id) {
-			latest_vid_id = out.items[0].id.videoId;
-		};
+		xyz = out.values;
 	});
 });
-
 
 client.on('message', async message => {
 	console.log(message.content);
@@ -36,23 +48,12 @@ client.on('message', async message => {
 	else if (message.content === prefix+`marcel`) message.channel.send('https://www.youtube.com/watch?v=RFWbO4hvsvA');
 	else if (message.content === prefix+`oh`) message.channel.send('https://www.youtube.com/watch?v=2KZuZknEI4c');
 	else if (message.content === prefix+`test2`) console.log(latest_vid_id);
+	else if (message.content === prefix+`test3`) message.channel.send(xyz);
 });
 
 
 client.on('message', function(message) {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	if (message.content === prefix+`test4`) {
-		let url = 'https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+process.env.SUMM_ID+'?api_key='+process.env.RIOT_TOKEN;
-		fetch(url)
-		.then(res => res.json())
-		.then((out) => {
-			for (i = 0; i < 999; i++) {
-				if (out[i].championLevel === 5) {
-					mastery5.push(out[i].championId);
-				};
-			};
-		});
-	};
 });
 
 
