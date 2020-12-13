dd6456bebafb54e7bbd18558e842c4db1b6d519b
MatzeR95 <matzer.95@gmail.com>
2020-12-13 18:20:49 +0100

    adadaasd

--- bot.js
+++ bot.js
@@ -13,22 +13,20 @@ client.once('ready', () => {
     console.log('Ready');
 });
 
-const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff');
-	.setTitle('Some title');
-	.setURL('https://discord.js.org/');
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org');
-	.setDescription('Some description here');
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png');
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	);
-	.addField('Inline field title', 'Some value here', true);
-	.setImage('https://i.imgur.com/wSTFkRM.png');
-	.setTimestamp();
+const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
 	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
 
